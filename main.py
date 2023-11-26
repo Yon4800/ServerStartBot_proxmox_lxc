986c0d9fc7eccf5790441896682d23a9a088b142
福知山よんぱちさん <48090196+Yon4800@users.noreply.github.com>
2023-11-26 03:17:22 +0000

    osからsubprocessに(動作未確認)

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import discord
 
 import pings
 
+import subprocess
+
 # .envファイルの内容を読み込見込む
 load_dotenv()
 
@@ -44,7 +46,7 @@ async def on_message(message):
     if message.content.startswith("$サーバーを停止して"):
         if res.is_reached():
             try:
-                os.system(
+                subprocess.check_call(
                     f"sshpass -p {os.environ['RPASSWD']} ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {os.environ['RUSERNAME']}@{os.environ['SSH']} 'echo {os.environ['RPASSWD']} | sudo -S shutdown -h now'"
                 )
                 await message.channel.send("サーバーを停止しました")
@@ -57,7 +59,7 @@ async def on_message(message):
     if message.content.startswith("$サーバーを再起動して"):
         if res.is_reached():
             try:
-                os.system(
+                subprocess.check_call(
                     f"sshpass -p {os.environ['RPASSWD']} ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {os.environ['RUSERNAME']}@{os.environ['SSH']} 'echo {os.environ['RPASSWD']} | sudo -S reboot'"
                 )
                 await message.channel.send("サーバーを再起動しました")
