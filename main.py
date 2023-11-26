7139bdd8b9ec8c50b61c171dd7c946ca6917fb49
福知山よんぱちさん <48090196+Yon4800@users.noreply.github.com>
2023-11-26 02:48:16 +0000

    インテンツを有効にしてみるとどうなるのだろう

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def on_message(message):
     # サーバー停止
     if message.content.startswith("$サーバーを停止して"):
         if res.is_reached():
-            await os.system(
+            os.system(
                 f"sshpass -p {os.environ['RPASSWD']} ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {os.environ['RUSERNAME']}@{os.environ['SSH']} 'echo {os.environ['RPASSWD']} | sudo -S shutdown -h now'"
             )
             await message.channel.send("サーバーを停止しました")
@@ -53,7 +53,7 @@ async def on_message(message):
     # サーバー再起動
     if message.content.startswith("$サーバーを再起動して"):
         if res.is_reached():
-            await os.system(
+            os.system(
                 f"sshpass -p {os.environ['RPASSWD']} ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {os.environ['RUSERNAME']}@{os.environ['SSH']} 'echo {os.environ['RPASSWD']} | sudo -S reboot'"
             )
             await message.channel.send("サーバーを再起動しました")
